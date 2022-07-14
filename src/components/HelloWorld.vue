73edd8302eae6d983f7738f1102f5bfc14919714
xzb <1968597503@qq.com>
2022-07-14 20:59:23 +0800

    页面样式变更

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <el-button type="primary" @click="getUserInfo">登录</el-button>
-    <div>{{userInfo}}</div>
+  <div class="page">
+    <div class="card">
+      <div class="card2">
+      </div>
+    </div>
+    <!-- <el-button type="primary" @click="getUserInfo">登录</el-button>
+    <div>{{userInfo}}</div> -->
   </div>
 </template>
 
@@ -30,4 +34,34 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.page{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #1a1a1a;
+  height: 100vh;
+}
+.card {
+  width: 400px;
+  height: 500px;
+  background-image: linear-gradient(163deg, #00ff75 0%, #3700ff 100%);
+  border-radius: 20px;
+  transition: all .3s;
+}
+.card2 {
+  width: 400px;
+  height: 500px;
+  background-color: #171717;
+  border-radius: 15px;
+  transition: all .2s;
+}
+
+.card2:hover {
+  transform: scale(0.98);
+  border-radius: 20px;
+}
+
+.card:hover {
+  box-shadow: 0px 0px 30px 1px rgba(0, 255, 117, 0.30);
+}
 </style>
