3e2ced62cfd938bc77b702516cd9134db68e9000
xzb <1968597503@qq.com>
2022-08-01 17:11:48 +0800

    路径修改

---
+++ src/page/home/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="page">
+    <div class="card">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import api from '@/api/index.js'
+export default {
+  data () {
+    return {
+      userInfo: {}
+    }
+  },
+  methods:{
+    // 获取用户信息
+    getUserInfo() {
+      api.getInfo({
+        id: '4'
+      }).then(response => {
+        this.userInfo = response.data
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+.page{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #1a1a1a;
+  height: 100vh;
+}
+</style>
